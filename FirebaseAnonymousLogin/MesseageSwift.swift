3a15f08321ae87310280329c442abeebb6173aba
Hiroki Kondo <kondo.matr02@gmail.com>
2019-12-23 19:48:37 +0900

    displayViewController実装

---
+++ FirebaseAnonymousLogin/MesseageSwift.swift
@@ -0,0 +1,16 @@
+//
+//  MesseageSwift.swift
+//  FirebaseAnonymousLogin
+//
+//  Created by 宏輝 on 23/12/2019.
+//  Copyright © 2019 宏輝. All rights reserved.
+//
+
+import Foundation
+
+class Message{
+    
+    var displayMessage1:String = ""
+    var displayMessage2:String = ""
+}
+
--- FirebaseAnonymousLogin/displayViewController.swift
+++ FirebaseAnonymousLogin/displayViewController.swift
@@ -7,15 +7,67 @@
 //
 
 import UIKit
+import Firebase
 
-class displayViewController: UIViewController {
-
+class displayViewController: UIViewController,UITableViewDelegate,UITableViewDataSource {
+    
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    var chatArray = [Message]()
+       
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
     
+    func numberOfSections(in tableView: UITableView) -> Int {
+        
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return chatArray.count
+    }
+
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+            
+        let displayMessageLabel1 = cell.contentView.viewWithTag(1) as! UILabel
+        let displayMessageLabel2 = cell.contentView.viewWithTag(2) as! UILabel
+        
+        cell.displayMessageLabel1.text = self.chatArray[indexPath.row].displayMessage1
+        cell.displayMessageLabel2.text = self.chatArray[indexPath.row].displayMessage2
+        
+            
+    }
+    
+    //firebaseからデータを引っ張ってくる(取得する）
+    func fetchChatData(){
+        
+        //どこからデータを引っ張ってくるのか
+        let fetchDataRef = Database.database().reference().child("chats")
+        
+        //新しく更新があったときだけ取得したい
+        fetchDataRef.observe(.childAdded) { (snapShot) in
+            
+            let snapShotData = snapShot.value as! AnyObject
+            let text = snapShotData.value(forKey: "message")
+            let sender = snapShotData.value(forKey: "sender")
+            
+            let message = Message()
+            message.message = text as! String
+            message.sender = sender as! String
+            
+            self.chatArray.append(message)
+            self.tableView.reloadData()
+        }
+    }
 
     /*
     // MARK: - Navigation
